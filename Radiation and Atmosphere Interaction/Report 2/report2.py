d360c6aeb14a85440f23a2bc5c4c9a2cf944d9a4
nadezsha <93947419+nadezsha@users.noreply.github.com>
2024-12-17 22:37:22 +0200

    plot more components and calculate mean values

--- Radiation and Atmosphere Interaction/Report 2/report2.py
+++ Radiation and Atmosphere Interaction/Report 2/report2.py
@@ -57,8 +57,6 @@ for i in range(0,6):
 
 #print(four_str[0])
 
-#percentages = pd.DataFrame()
-
 for i in range(0,6):
 	sixteen_str[i]["Actinic_2_16"] = (two_str[i]['Actinic_flux'] - sixteen_str[i]['Actinic_flux'])/sixteen_str[i]['Actinic_flux'] *100
 	sixteen_str[i]["Actinic_4_16"] = (four_str[i]['Actinic_flux'] - sixteen_str[i]['Actinic_flux'])/sixteen_str[i]['Actinic_flux'] *100
@@ -69,7 +67,7 @@ for i in range(0,6):
 	sixteen_str[i]["Global_8_16"] = (eight_str[i]['Global'] - sixteen_str[i]['Global'])/sixteen_str[i]['Global'] *100
 
 print(sixteen_str[0])
-
+'''
 for i in range(0,6):
 	plt.plot(sixteen_str[i]["Wavelength"], sixteen_str[i]["Actinic_2_16"], color ='b', label = "2 streams")
 	plt.plot(sixteen_str[i]["Wavelength"], sixteen_str[i]["Actinic_4_16"], color ='r', label = "4 streams")
@@ -91,3 +89,48 @@ for i in range(0,6):
 	plt.grid()
 	plt.title("Global irradiance for sza = " +str(sza_list[i])+" degrees")
 	plt.show()
+'''
+
+# extra stuff : find the % differences for the direct and diffused irradiance
+for i in range(0,6):
+	sixteen_str[i]["direct_2_16"] = (two_str[i]['Direct'] - sixteen_str[i]['Direct'])/sixteen_str[i]['Direct'] *100
+	sixteen_str[i]["direct_4_16"] = (four_str[i]['Direct'] - sixteen_str[i]['Direct'])/sixteen_str[i]['Direct'] *100
+	sixteen_str[i]["direct_8_16"] = (eight_str[i]['Direct'] - sixteen_str[i]['Direct'])/sixteen_str[i]['Direct'] *100
+
+	sixteen_str[i]["diffused_2_16"] = (two_str[i]['Diffuse_Down'] - sixteen_str[i]['Diffuse_Down'])/sixteen_str[i]['Diffuse_Down'] *100
+	sixteen_str[i]["diffused_4_16"] = (four_str[i]['Diffuse_Down'] - sixteen_str[i]['Diffuse_Down'])/sixteen_str[i]['Diffuse_Down'] *100
+	sixteen_str[i]["diffused_8_16"] = (eight_str[i]['Diffuse_Down'] - sixteen_str[i]['Diffuse_Down'])/sixteen_str[i]['Diffuse_Down'] *100
+
+
+for i in range(0,6):
+	plt.plot(sixteen_str[i]["Wavelength"], sixteen_str[i]["direct_2_16"], color ='b', label = "2 streams")
+	plt.plot(sixteen_str[i]["Wavelength"], sixteen_str[i]["direct_4_16"], color ='r', label = "4 streams")
+	plt.plot(sixteen_str[i]["Wavelength"], sixteen_str[i]["direct_8_16"], color ='g', label = "8 streams")
+	plt.xlabel(" Wavelength (nm)")
+	plt.ylabel("'%' differences relative to the reference (16 str)")
+	plt.legend()
+	plt.grid()
+	plt.title("direct irradiance for sza = "+str(sza_list[i])+" degrees")
+	plt.show()
+
+for i in range(0,6):
+	plt.plot(sixteen_str[i]["Wavelength"], sixteen_str[i]["diffused_2_16"], color ='b', label = "2 streams")
+	plt.plot(sixteen_str[i]["Wavelength"], sixteen_str[i]["diffused_4_16"], color ='r', label = "4 streams")
+	plt.plot(sixteen_str[i]["Wavelength"], sixteen_str[i]["diffused_8_16"], color ='g', label = "8 streams")
+	plt.xlabel(" Wavelength (nm)")
+	plt.ylabel("'%' differences relative to the reference (16 str)")
+	plt.legend()
+	plt.grid()
+	plt.title("diffused irradiance for sza = " +str(sza_list[i])+" degrees")
+	plt.show()
+
+# πινακάκια με αποκλίσεις : 3 στριμς * 4 μεγεθη * 6 ζενιθιες = 72 πινακακια... to be continued...
+for i in range(0,6):
+	ranges = [
+    ("uvb", sixteen_str[i][sixteen_str[i]["Wavelength"] < 315]['diffused_2_16'].mean()),
+    ("uva", sixteen_str[i][(sixteen_str[i]["Wavelength"] >= 315) & (sixteen_str[i]["Wavelength"] < 400)]['diffused_2_16'].mean()),
+    ("vis", sixteen_str[i][(sixteen_str[i]["Wavelength"] >= 400) & (sixteen_str[i]["Wavelength"] < 800)]['diffused_2_16'].mean()),
+    ("ir", sixteen_str[i][sixteen_str[i]["Wavelength"] >= 800]['diffused_2_16'].mean()),
+	]
+	result = pd.DataFrame(ranges)
+	print(result)
