33d8e8e9a041872dfd0ac3268fec9a2fb0943ca9
nadezsha <93947419+nadezsha@users.noreply.github.com>
2025-01-21 11:03:51 +0200

    delete directory because i fd up

--- Radiation and Atmosphere Interaction/Report 4/PART_IV/bat.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Loop through all .inp files in the current directory and run uvspec for each one
-for %%f in (*.inp) do (
-    echo Running uvspec for file %%f...
-    uvspec < "%%f" > "%%~nf.out"
-)
-
-echo All simulations are complete!
-pause
