8621a2fd7373beba9d211da4998d8a526753aa69
nadezsha <93947419+nadezsha@users.noreply.github.com>
2025-01-21 10:21:10 +0200

    update bat.bat

--- Radiation and Atmosphere Interaction/Report 4/PART_IV/bat.bat
+++ Radiation and Atmosphere Interaction/Report 4/PART_IV/bat.bat
@@ -1,17 +1,8 @@
 @echo off
-:: Set the path to Libradtran executable if it's not in the system PATH
-set LIBRADTRAN_PATH=C:\path\to\libradtran\bin
-
-:: Set the directory containing your input files (generated_files from the Python script)
-set INPUT_DIR=generated_files
-
-:: Change to the directory containing the input files (optional, for convenience)
-cd %INPUT_DIR%
-
-:: Loop through all the .dat files and run uvspec for each one
-for %%f in (*.dat) do (
+:: Loop through all .inp files in the current directory and run uvspec for each one
+for %%f in (*.inp) do (
     echo Running uvspec for file %%f...
-    "%LIBRADTRAN_PATH%\uvspec" < %%f > %%~nf_output.txt
+    uvspec < "%%f" > "%%~nf.out"
 )
 
 echo All simulations are complete!
